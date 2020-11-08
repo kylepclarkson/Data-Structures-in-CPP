a7453234f93847eb643f56a39306a086afe5eee4
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-11-08 13:10:36 -0800

    Created repo.

---
+++ testing/inout.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Kyle on 2020-11-08.
+//
+
+#include <iostream>
+
+int main() {
+
+    int x, y;
+    std::cout << "Enter two numbers: ";
+    std::cin >> x >> y;
+
+    int sum = x + y;
+    std::cout << "There sum is: " << sum << std::endl;
+    return 0;
+}
